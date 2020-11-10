50ebf998c69f1aaf06b4b2292c50c1ae1c3468ae
Madi <madi.turlubekov@mail.ru>
2020-11-10 11:50:55 +0600

    update js file

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Я тебя еще раз спрашиваю! Сколько фильмов вы уже просмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,27 +20,51 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько вы его оцените?', '');
 
-    if (a != null && b != null && a != '' && b!= '' && a.length < 50) {
-        personalMovieDB.movies[a] = b; 
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько вы его оцените?');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }else {
+            console.log('error');
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("malo");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("classic");
-} else if (personalMovieDB.count >= 30) {
-    console.log("kinaman");
-} else {
-    console.log('Error');
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Malo!');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+        console.log('Normalno!');
+    } else if (personalMovieDB >= 30) {
+        console.log('Nihuya ti kinoman');
+    } else {
+        console.log('BLyat');
+    }
 }
 
-console.log(personalMovieDB);
+// detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGeners() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGeners();
